7071b54d3988af9a4f6eb015de9479607b422621
HSWilson3 <101376727+HSWilson3@users.noreply.github.com>
2025-02-26 22:10:50 -0600

    Create rough calculator display

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const numA = 0;
+const numB = 0;
+const operator = 0;
+
 function add(a, b) {
     return a + b;
 }
@@ -12,5 +16,24 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(op, a, b) {
+    switch (op) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break;
+        default:
+            return "invalid operator";
+    }
 }
 
